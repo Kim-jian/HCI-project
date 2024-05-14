47810a1d874220b980faaa1d404f5d19f2d9d4c0
Kimjian <ekekekrekr@naver.com>
2024-05-14 12:19:28 +0900

    '최근 수정 순' => '최근 열람 순' 으로 단어 변경. 따라서 '업로드 순','최근 열람 순' 2가지 홈화면 대본 정렬모드가 존재

--- lib/ScriptManager.dart
+++ lib/ScriptManager.dart
@@ -6,31 +6,31 @@ class ScriptManager{
   SettingEnvironmentController _settingEnvironmentController = SettingEnvironmentController();
   List<Script> scriptList = [
     Script(
-      title: '찌안이꼬츄',
+      title: '1',
       content: 'This is the content of script 1.',
       date: '2023.05.10',
       latestdate: '2023.05.19',
     ),
     Script(
-      title: '뱁배개벅벅',
+      title: '2',
       content: 'This is the content of script 2.',
       date: '2023.05.11',
       latestdate: '2023.05.18',
     ),
     Script(
-      title: '아냐포져',
+      title: '3',
       content: 'This is the content of script 3.',
       date: '2023.05.15',
       latestdate: '2023.05.17',
     ),
     Script(
-      title: '계란말이요요',
+      title: '4',
       content: 'This is the content of script 4.',
       date: '2023.05.14',
       latestdate: '2023.05.16',
     ),
     Script(
-      title: '뱁배개먹고싶오요',
+      title: '5',
       content: 'This is the content of script 5.',
       date: '2023.05.13',
       latestdate: '2023.05.15',
